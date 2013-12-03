9a9b77a4f069bbca01b17fd99e3b67cd118cff16
Jerrett <jah555@cornell.edu>
2013-12-03 15:55:56 -0500

    adds functionality to update_foc and add_charge in team.ml

--- game/team.ml
+++ game/team.ml
@@ -5,7 +5,19 @@ open Constants
 open Util
 
 let update_foc (f_bool : bool) (team : team_data) : team_data =
+  let (lives, bombs, score, power, charge, player) = team in
+  let player' = {player with p_focused = f_bool} in
+  (*Return the new team_data with updated player = player'*)
+  (lives, bombs, score, power, charge, player')
 
 let update_pos (dir_lst : direction list) (team : team_data) : team_data =
+  failwith("Ocaml is the best")
 
 let add_charge (team : team_data) : team_data = 
+  let (lives, bombs, score, power, charge, player) = team in
+  let charge' = charge + cCHARGE_RATE in
+  if charge' > cCHARGE_MAX then
+    (lives, bombs, score, power, cCHARGE_MAX, player)
+  else
+    (lives, bombs, score, power, charge', player)
+end
