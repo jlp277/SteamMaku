639e5a683d94b5a8f01449db61123a67601968b6
James Park <jlp277@cornell.edu>
2013-12-03 20:58:49 -0500

    Fixes spacing on bullet.ml

--- game/bullet.ml
+++ game/bullet.ml
@@ -4,4 +4,9 @@ let check_contacts (data : game_data) : player_char list * player_char list =
   (* check if invincible *)
   (* output should have no duplicate players *)
   failwith "lkdfj"
+
+(* update bullet position and velocity *)
+let update (bullets : bullet list) : bullet list =
+  failwith "dlkfj"
+
 end
