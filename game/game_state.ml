69a483eed3215a25cc56c5de411288bafc2812d2
James Park <jlp277@cornell.edu>
2013-12-03 21:14:17 -0500

    Implements effects of getting shot and scoring.

--- game/game_state.ml
+++ game/game_state.ml
@@ -5,14 +5,22 @@ open Constants
 open Util
 
 let decrement_lives (team : team_data) : team_data =
+
   match team with
   | (lives,bombs,score,power,charge,player) ->
     (lives-1,cINITIAL_BOMBS,score,power,charge,player)
 
 let victim (team : team_data) : team_data =
-  (* remove life *)
+  match team with
+  | (lives,bomb,score,power,charge,player) ->
+    (lives - 1,cINITIAL_BOMBS,score,power/2,charge,player)
+  | _ -> failwith "bad team_data in handle_focus"
 
 let shooter (team : team_data) : team_data = 
+  match team with
+  | (lives,bomb,score,power,charge,player) ->
+    (lives,bomb,score+cKILL_POINTS,power,charge,player)
+  | _ -> failwith "bad team_data in handle_focus"
 
 let handle_move game col dir_lst =
 
@@ -39,7 +47,6 @@ let handle_bomb game col =
     { game with data = data'; red_inv = cBOMB_DURATION; red_bomb = true }
   else
     { game with data = data'; blue_inv = cBOMB_DURATION; blue_bomb = true }
-  
 
 let check_result (data: game_data) (duration: float) : result =
   failwith "Picasso was the man"
